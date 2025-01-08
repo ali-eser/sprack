aa374dd3f934c418d4274c7bb52690999d75b0da
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-01-09 01:12:19 +0300

    Initial vite setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Sprack!</h1>
+      <form action="submit">
+        <textarea cols={120} rows={5} name="text" id="text" placeholder='Input text'></textarea>
+      </form>
+    </>
+  )
+}
+
+export default App
